35be4169b09df4793c1370615d1f7c75e83b0557
Matthew Cook <matthewcook13987@gmail.com>
2021-03-31 20:51:01 +0000

    adds instance variables of product and category count to be displayed in the admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,8 +2,9 @@ class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
-  
+
   def show
-   #puts "spaghetti"
+   @product_num = Product.all.count()
+   @category_num = Category.all.count()
   end
 end

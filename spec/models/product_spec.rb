6a0747252e64dea36d757286516f7c8301c1cc8a
Matthew Cook <matthewcook13987@gmail.com>
2021-04-05 20:22:55 +0000

    Adds more validation tests to products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Product, type: :model do
 
   describe 'Validations' do
 
-    it 'should validate with correct values' do
+    it 'should validate with a proper values' do
       @category = Category.new(name: 'Houseplants')
       @product = Product.new(
         name: 'Flowers',
@@ -17,6 +17,73 @@ RSpec.describe Product, type: :model do
       @product.save!
 
       expect(@product).to be_valid
+    end
+
+
+    it 'should be invalid if no name is provided' do
+      @category = Category.new(name: 'Houseplants')
+      @product = Product.new(
+        name: nil,
+        category: @category,
+        description: 'nice flowers',
+        quantity: 5,
+        price: 500,
+        image: 'https://i.imgur.com/bFDWhkK.jpg'
+      )
+      @product.save
+
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to eq(["Name can't be blank"])
+    end
+
+
+    it 'should be invalid if no price is provided' do
+      @category = Category.new(name: 'Houseplants')
+      @product = Product.new(
+        name: 'Flowers',
+        category: @category,
+        description: 'nice flowers',
+        quantity: 5,
+        price: nil,
+        image: 'https://i.imgur.com/bFDWhkK.jpg'
+      )
+      @product.save
+
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to eq(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
+    end
+
+
+    it 'should be invalid if no quanitity is provided' do
+      @category = Category.new(name: 'Houseplants')
+      @product = Product.new(
+        name: 'Flowers',
+        category: @category,
+        description: 'nice flowers',
+        quantity: nil,
+        price: 500,
+        image: 'https://i.imgur.com/bFDWhkK.jpg'
+      )
+      @product.save
+
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to eq(["Quantity can't be blank"])
+    end
+
+    it 'should be invalid if no category is provided' do
+      @category = Category.new(name: 'Houseplants')
+      @product = Product.new(
+        name: 'Flowers',
+        category: nil,
+        description: 'nice flowers',
+        quantity: 10,
+        price: 500,
+        image: 'https://i.imgur.com/bFDWhkK.jpg'
+      )
+      @product.save
+
+      expect(@product).not_to be_valid
+      expect(@product.errors.full_messages).to eq(["Category can't be blank"])
     end
 
   

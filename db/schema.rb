096bb5b60d61a9c9bbae7d9d074d421708af8ad9
Matthew Cook <matthewcook13987@gmail.com>
2021-04-05 19:29:14 +0000

    installed rpsec for testing

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20210401162424) do
-=======
 ActiveRecord::Schema.define(version: 20210401175429) do
->>>>>>> feature/user-auth
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

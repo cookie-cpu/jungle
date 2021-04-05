f5b5cae9336de6fbe856ac4e24917171d34df4ff
Matthew Cook <matthewcook13987@gmail.com>
2021-04-05 21:03:17 +0000

    adds more specs to validate users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,32 @@
 class User < ActiveRecord::Base
+
+  before_validation :format_email
+
+  validates :password, presence: true, length: {minimum: 5 }
+  validates :password_confirmation, presence: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, presence: true
+  validates_uniqueness_of :email, :case_sensitive => false
+
   has_secure_password
+
+  def self.authenticate_with_credentials (email, password)
+    email = email.strip unless email.nil?
+    email = email.downcase unless email.nil?
+
+    user = self.find_by_email(email)
+
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
+  private
+    def format_email 
+      self.email = email.downcase
+    end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'Validations' do
 
 
     it "should save a user if given valid attributes" do 
@@ -16,8 +17,64 @@ RSpec.describe User, type: :model do
     end
 
 
+    it "should be invalid if the email already exists" do 
+      @user = User.new(
+        firstname: "Matthew",
+        lastname: "Cook",
+        email: "test@test.com", 
+        password: "password",
+        password_confirmation: "password",
+      )
+      @user.save
+
+      @user2 = User.new(
+        firstname: "Matthew",
+        lastname: "Cook",
+        email: "TEST@TEST.com", 
+        password: "password",
+        password_confirmation: "password",
+      )
+      @user2.save
 
 
+      expect(@user2).not_to be_valid
+    end
 
-  
+
+    it "should be invalid if password is less than 5 characters" do 
+      @user = User.new(
+        firstname: "Matthew",
+        lastname: "Cook",
+        email: "MATTHEW@gmail.com", 
+        password: "123",
+        password_confirmation: "123",
+      )
+
+      @user.save
+
+      expect(@user).not_to be_valid
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+
+    
+    it 'should validate with valid credentials' do
+      @user = User.new(
+        firstname: "Matthew",
+        lastname: "Cook",
+        email: "matthew@gmail.com", 
+        password: "password",
+        password_confirmation: "password",
+      )
+      @user.save
+
+      logInInfo = User.authenticate_with_credentials(@user.email, @user.password)
+
+      expect(logInInfo.id).to eq(@user.id)
+    end
+    
+
+
+  end
 end
